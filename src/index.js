3fc8f98239a4444d64c71fad8e8395223c682bb1
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-01-11 22:04:14 +0200

    adds redux-persist removal

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
-import { persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
- 
